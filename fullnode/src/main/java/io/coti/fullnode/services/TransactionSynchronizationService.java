2ccc260bc7f397db598e0c86997b9c2029cf7cf7
cotitech <eli@coti.io>
2021-04-08 19:59:52 +0300

    refactor at insert missing txs

--- fullnode/src/main/java/io/coti/fullnode/services/TransactionSynchronizationService.java
+++ fullnode/src/main/java/io/coti/fullnode/services/TransactionSynchronizationService.java
@@ -7,6 +7,7 @@ import io.coti.basenode.services.BaseNodeTransactionSynchronizationService;
 import io.coti.basenode.services.interfaces.ITransactionService;
 import io.coti.fullnode.data.AddressTransactionsByAttachment;
 import io.coti.fullnode.model.AddressTransactionsByAttachments;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,10 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
 
 @Service
+@Slf4j
 public class TransactionSynchronizationService extends BaseNodeTransactionSynchronizationService {
 
     @Autowired
@@ -27,26 +30,22 @@ public class TransactionSynchronizationService extends BaseNodeTransactionSynchr
 
     @Override
     protected void insertMissingTransactions(List<TransactionData> missingTransactions, Set<Hash> trustChainUnconfirmedExistingTransactionHashes, AtomicLong completedMissingTransactionNumber, AtomicBoolean finishedToReceive, int offset) {
-        int missingTransactionsSize;
-        int nextOffSet;
         Map<Hash, AddressTransactionsHistory> addressToTransactionsHistoryMap = new ConcurrentHashMap<>();
         Map<Hash, AddressTransactionsByAttachment> addressToTransactionsByAttachmentMap = new ConcurrentHashMap<>();
+        Consumer<TransactionData> handleTransactionConsumer = transactionData -> {
+            transactionService.handleMissingTransaction(transactionData, trustChainUnconfirmedExistingTransactionHashes, missingTransactionExecutorMap);
+            transactionHelper.updateAddressTransactionHistory(addressToTransactionsHistoryMap, transactionData);
+            ((TransactionHelper) transactionHelper).updateAddressTransactionByAttachment(addressToTransactionsByAttachmentMap, transactionData);
+        };
+        handleMissingTransactions(missingTransactions, handleTransactionConsumer, completedMissingTransactionNumber, finishedToReceive, offset);
 
-        while ((missingTransactionsSize = missingTransactions.size()) > offset || !finishedToReceive.get()) {
-            if (missingTransactionsSize - 1 > offset || (missingTransactionsSize - 1 == offset && missingTransactions.get(offset) != null)) {
-                nextOffSet = offset + (finishedToReceive.get() ? missingTransactionsSize - offset : 1);
-                for (int i = offset; i < nextOffSet; i++) {
-                    TransactionData transactionData = missingTransactions.get(i);
-                    transactionService.handleMissingTransaction(transactionData, trustChainUnconfirmedExistingTransactionHashes, missingTransactionExecutorMap);
-                    transactionHelper.updateAddressTransactionHistory(addressToTransactionsHistoryMap, transactionData);
-                    ((TransactionHelper) transactionHelper).updateAddressTransactionByAttachment(addressToTransactionsByAttachmentMap, transactionData);
-                    missingTransactions.set(i, null);
-                    completedMissingTransactionNumber.incrementAndGet();
-                }
-                offset = nextOffSet;
-            }
-        }
-        addressTransactionsHistories.putBatch(addressToTransactionsHistoryMap);
+        insertAddressTransactionsHistory(addressToTransactionsHistoryMap);
+        insertAddressTransactionsByAttachment(addressToTransactionsByAttachmentMap);
+    }
+
+    private void insertAddressTransactionsByAttachment(Map<Hash, AddressTransactionsByAttachment> addressToTransactionsByAttachmentMap) {
+        log.info("Started to insert address transactions by attachment");
         addressTransactionsByAttachments.putBatch(addressToTransactionsByAttachmentMap);
+        log.info("Finished to insert address transactions by attachment");
     }
 }
